2918b939e4db06f61df1560c5385f6f052df1874
davmz <74930417+davmz@users.noreply.github.com>
2021-07-08 17:57:39 -0500

    MadLib Template Selector/ Simple Madlib
    creation of a more sophisticated madlib and a simple

--- MadLibs#2/MadLibs.py
+++ MadLibs#2/MadLibs.py
@@ -1,79 +1,11 @@
 """
 Name: David Montanez
 Reason: Hello World
-Created: July 7, 2021
-Updated: July 7, 2021
+Created: July 8, 2021
+Updated: July 8, 2021
 """
 
-import json
-import os
+name = input("Enter in a name: ")
+madlib = f"My name is {name}"
 
-# # string concatenation (aka how to put strings together)
-
-# ## Introduction: create a string => "My name is ____"
-# name = "David Montanez"
-
-# # Few ways to do this:
-# print("My name is " + name)
-# print("My name is {}".format(name))
-# print(f"My name is {name}\n")
-
-# ## Program:
-# adj = input("Adjective: ")
-# verb1 = input("Verb: ")
-# verb2 = input("Verb: ")
-# person = input("Person: ")
-
-# mablib = f"Computer programming is so {adj}!\n It makes me so excited all the time because \
-# I love to {verb1}.\n Stay hydrated and {verb2} like you are {person}!"
-
-# print(mablib)
-
-## Program:
-class MadLibs:
-    path = ".\\templates"
-    def __init__(self, wordDescriptions, template):
-        self.template = template
-        self.wordDescriptions = wordDescriptions
-        self.userInput = []
-        self.story = None
-
-    @classmethod
-    def fromJSON(cls, name, path = None):
-        if not path:
-            path = cls.path
-        filePath = os.path.join(path, name)
-
-        with open(filePath, "r") as f:
-            data = json.load(f)
-
-        madLib = cls(**data)
-
-        return madLib
-
-    def getWordsFromUser(self):
-        print("Please provide the following words: ")
-
-        for desc in self.wordDescriptions:
-            ui = input(desc + ": ")
-            self.userInput.append(ui)
-
-        return self.userInput
-
-    def buildStory(self):
-        self.story = self.template.format(*self.userInput)
-
-        return self.story
-
-def selectTemplate():
-    templates = os.listdir(MadLibs.path)
-    print(templates)
-
-selectTemplate()
-
-# templateName = "DayAtZoo.json"
-# madlib = MadLibs.fromJSON(templateName)
-# words = madlib.getWordsFromUser()
-# story = madlib.buildStory()
-
-# print(story)
+print(f"MadLib: {madlib}")
