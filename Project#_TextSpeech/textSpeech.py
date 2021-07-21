8adf8f21a5f0f41dee23d9ca61aefb46be354d15
davmz <74930417+davmz@users.noreply.github.com>
2021-07-21 16:51:13 -0500

    text speech recognition

--- Project#_TextSpeech/textSpeech.py
+++ Project#_TextSpeech/textSpeech.py
@@ -8,31 +8,88 @@ Updated: July 21, 2021
 ## Packages
 # pip install pipwin => PyAudio download errors
 # pip install wheel => needed to download PyAudio package
-# pipwin install pyaudio
+# pipwin install pyaudio or pip install PyAudio
 # https://stackoverflow.com/questions/53866104/pyaudio-failed-to-install-windows-10
 
 ## Imports
+import os
+clear = lambda: os.system("cls")
+
 # pip install SpeechRecognition
+import speech_recognition as sr
 
 # pip install pyttsx3
+import pyttsx3
 
-
-
-# pip install PyAudio
+# pip install python-vlc
 
 ## Program 
 def speech():
-    r = sr.Recognizer()
+    recognizer = sr.Recognizer()
+    voiceEngine = pyttsx3.init()
+    voiceEngine.setProperty("rate", 190)
 
-    with sr.Microphone() as source:
-        print("Speak Anything: ")
-        audio = r.listen(source)
+    # voiceEngine.say("I am Jarvis. Your AI Helper!")
+    # voiceEngine.say("How can I help you?")
+    # voiceEngine.runAndWait()
 
-        text = r.recognize_google(audio)
+    while True:
+        try:
+            with sr.Microphone() as source:
+                
+                recognizer.adjust_for_ambient_noise(source, duration = 0.2)
+                audio = recognizer.listen(source)
 
-        print(text)
+                text = recognizer.recognize_google(audio)
+                text = text.lower()
+                print(text)
 
+                if("hey google" in text):
+                    voiceEngine.say("How can I help you... Question mark!")
+                    voiceEngine.runAndWait()
+
+                if("my name is" in text):
+                    name = text.split()
+                    voiceEngine.say(f"Your name is {name[-1]}")
+                    voiceEngine.runAndWait()
+
+                if("clap" in text):
+
+                
+                voiceEngine.say(text)
+                voiceEngine.runAndWait()
+        except:
+            recognizer = sr.Recognizer()
+            continue
+
+def test():
+    recognizer = sr.Recognizer()
+    voiceEngine = pyttsx3.init()
+    voiceEngine.setProperty("rate", 190)
+
+    voiceEngine.say("I am Jarvis. Your AI Helper!")
+    voiceEngine.say("How can I help you?")
+    voiceEngine.runAndWait()
+
+    voice = voiceEngine.getProperty("voices")
+    for i in voice:
+        print(i)
+
+    # while True:
+    #     try:
+    #         with sr.Microphone() as source:
+                
+    #             recognizer.adjust_for_ambient_noise(source, duration = 0.2)
+    #             audio = recognizer.listen(source)
+
+    #             text = recognizer.recognize_google(audio)
+
+    #             print(text)
+    #     except:
+    #         recognizer = sr.Recognizer()
+    #         continue
 
 if(__name__ == "__main__"):
     speech()
+    # test()
 
